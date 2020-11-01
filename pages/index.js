dd49931ab620375eb08d54de590a7f5186d875c4
fulljames <stephen@fulljames.net>
2020-11-01 23:37:48 +0000

    Make the entire website

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,53 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+
+const analytics = process.env.NODE_ENV === 'development' ? null : (
+  <>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-12209334-1"></script>
+    <script dangerouslySetInnerHTML={{
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-12209334-1');`
+    }}/>
+  </>
+)
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
+        { analytics }
+        <title>Turkey Leek and Stilton Soup</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main>
+        <h1>Turkey Leek and Stilton Soup</h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <ol>
+          <li>Acquire as much leftover sliced turkey as physically possible.</li>
+          <li>For <em>each</em> 325g (11oz) of turkey you will need:
+            <ul>
+              <li>4 tbsp butter</li>
+              <li>1 large onion, chopped</li>
+              <li>1 leek leek, trimmed and sliced</li>
+              <li>600ml (1pt) chicken stock</li>
+              <li>150g (5.5oz) Stilton</li>
+              <li>150ml (5fl oz) double cream</li>
+              <li>1 tbsp chopped fresh tarragon</li>
+            </ul>
+          </li>
+          <li>Melt the butter in a large pan and cook the onion for 4 mins until slightly softened</li>
+          <li>Add the leek and cook for a further 3 mins</li>
+          <li>Add the stock and turkey and bring to the boil, then reduce the heat and simmer for 15 mins</li>
+          <li>Remove from the heat and leave to cool</li>
+          <li>Blend coarsely with a stick blender (or remove half and blend in a food processor and return to the pan</li>
+          <li>Add the Stilton, cream and tarragon, season to taste and reheat gently</li>
+          <li>Serve garnished with tarragon leaves and croutons</li>
+        </ol>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
